f6c30703e0237d1c2d784a6ecb35491b53b15379
Jay Wang <532655203@qq.com>
2022-02-12 16:47:37 +0800

    实现懒加载

--- page_lifecycle/src/main/java/com/jj/page_lifecycle/base/BaseViewPager2Page.java
+++ page_lifecycle/src/main/java/com/jj/page_lifecycle/base/BaseViewPager2Page.java
@@ -12,6 +12,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 public abstract class BaseViewPager2Page<T> extends ConstraintLayout {
 
     private boolean isConvertedOnceTime;
+    private boolean isPaused;
 
     public BaseViewPager2Page(Context context) {
         this(context, null);
@@ -25,13 +26,21 @@ public abstract class BaseViewPager2Page<T> extends ConstraintLayout {
         super(context, attrs, defStyleAttr);
     }
 
-    public void convert(T bean, int adapterPosition) {
-        if (isConvertedOnceTime) {
-            onClearOldPageData();
-        } else {
-            isConvertedOnceTime = true;
+    public void pause() {
+        if (isPaused) {
+            return;
         }
-        onConvertView(bean, adapterPosition);
+        isPaused = true;
+        onPause();
+        onSavePageData();
+    }
+
+    protected void clearOldPageData() {
+        if (!isConvertedOnceTime) {
+            isConvertedOnceTime = true;
+            return;
+        }
+        onClearOldPageData();
     }
 
     /**
@@ -42,30 +51,46 @@ public abstract class BaseViewPager2Page<T> extends ConstraintLayout {
     /**
      * 挂载新条目数据
      */
-    protected abstract void onConvertView(T bean, int adapterPosition);
+    protected abstract void onViewConverted(T bean, int adapterPosition);
+
+    public void resume() {
+        isPaused = false;
+        onResume();
+    }
 
     /**
      * 从预加载范围滑到可见条目
      * 此时动画/工作线程等耗费资源的工作应开启
      */
-    public abstract void onResume();
+    protected abstract void onResume();
 
     /**
      * 从可见条目滑到预加载范围
      * 此时动画/工作线程等耗费资源的工作应关闭
      */
-    public abstract void onPause();
+    protected abstract void onPause();
 
+    /**
+     * 从预加载范围到回收池,或viewpager2销毁
+     */
     @Override
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
-        onSavePageData();
+        //viewpage2销毁时,最后一个选中条目在此回调pause
+        pause();
+        //所有条目都需要清空缓存
+        onRelease();
     }
 
     /**
-     * 退到预加载范围之外
-     * 此时该条目产生的用户数据应保存好,因为这之后,viewPager2.findViewByTag(int)将找不到本view了
+     * 条目进入预加载范围,或进入回收池
+     * 此时该条目产生的用户数据应保存好,因为一旦进入回收池,viewPager2.findViewByTag(int)将找不到本view了
      */
     public abstract void onSavePageData();
+
+    /**
+     * 条目控件销毁时机,必须防止内存泄漏
+     */
+    protected abstract void onRelease();
 
 }
